0b3055d4b1d8f05c0f0972a135df0485efc1aade
IsabellyCavalcante <isabelly.cavalcante@ccc.ufcg.edu.br>
2019-08-31 00:10:00 -0300

    corigindo paths

--- src/scripts/getOriginal.py
+++ src/scripts/getOriginal.py
@@ -1,16 +1,22 @@
 from xml.dom import minidom
+import sys
 
-xml_doc = minidom.parse('dados/execution_time.xml')
-test_list = xml_doc.getElementsByTagName('testcase')
+def main(post):
+    xml_doc = minidom.parse('../dados/execution_time-{}.xml.format(post)')
+    test_list = xml_doc.getElementsByTagName('testcase')
 
-with open('dados/original.txt', 'w') as out:
-	for test in test_list:
-		meth = test.attributes['name'].value.split(" ")[0]
-		test_case = test.attributes['classname'].value
-		
-		tmp = u' '.join((test_case, meth)).encode('utf-8')
+    with open('../dados/output/original-{}.txt'.format(post), 'w') as out:
+        for test in test_list:
+            meth = test.attributes['name'].value.split(" ")[0]
+            test_case = test.attributes['classname'].value
 
-		out.write(tmp)
-		out.write('\n')        
+            tmp = u' '.join((test_case, meth)).encode('utf-8')
 
-print "fim"
+            out.write(tmp)
+            out.write('\n')
+
+    print ("fim")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
