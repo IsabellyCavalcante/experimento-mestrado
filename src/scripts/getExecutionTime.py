d1de533e4bba06c8bd50aa0a214bc7edce4c572d
IsabellyCavalcante <isabelly.cavalcante@ccc.ufcg.edu.br>
2019-08-30 23:45:56 -0300

    corrigindo script de getExecutionTime para funcionar no script de automatizacao

--- src/scripts/getExecutionTime.py
+++ src/scripts/getExecutionTime.py
@@ -1,25 +1,30 @@
 from xml.dom import minidom
+import sys
 
-xml_doc = minidom.parse('dados/execution_time.xml')
-test_list = xml_doc.getElementsByTagName('testcase')
+def main(post):
+    xml_doc = minidom.parse('../dados/execution_time-{}.xml'.format(post))
+    test_list = xml_doc.getElementsByTagName('testcase')
 
-dict_test = {}
+    dict_test = {}
 
-for test in test_list:
-    meth = test.attributes['name'].value.split(" ")[0]
-    test_case = test.attributes['classname'].value
-    
-    tmp = u' '.join((test_case, meth))
-    
-    dict_test[tmp] = test.attributes['time'].value
+    for test in test_list:
+        meth = test.attributes['name'].value.split(" ")[0]
+        test_case = test.attributes['classname'].value
+        
+        tmp = u' '.join((test_case, meth))
+        
+        dict_test[tmp] = test.attributes['time'].value
 
-with open('dados/tests.txt', 'rb') as tests, open('dados/times.txt', 'w') as out:
-    
-    file_tests = tests.readlines()
-    
-    for name in file_tests:
-        time = dict_test[name.decode('CP1252').rstrip()]
-        out.write(time)
-        out.write('\n')
+    with open('../dados/tests-{}.txt'.format(post), 'rb') as tests, open('../dados/times-{}.txt'.format(post), 'w') as out:
+        
+        file_tests = tests.readlines()
+        
+        for name in file_tests:
+            time = dict_test[name.decode('CP1252').rstrip()]
+            out.write(time)
+            out.write('\n')
 
-print "fim"
+    print ("fim")
+
+if __name__ == "__main__":
+    main(sys.argv[1])
