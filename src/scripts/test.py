be684c924d2a1cd245a97100608d7e4b60ec4a35
IsabellyCavalcante <isabelly.cavalcante@ccc.ufcg.edu.br>
2019-09-08 22:19:54 -0300

    correcao script de get nomes dos testes

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,49 +1,63 @@
-with open('../dados/Priorizacao_do_exp1.txt', 'r') as tests:
-    
-    file_tests = tests.readlines()
+import sys
 
-    with open('../dados/tests.txt', 'r') as names:
-        
-        file_names = names.readlines()
-        
-        #Total
-        with open('../dados/total_final.txt', 'w') as out:
-            prio = file_tests[3].split(", ")
 
-            for i in prio:
-                out.write(file_names[int(i)])
+def main():
+    post = sys.argv[1]
+    with open('../dados/Priorizacao_{}.txt'.format(post), 'r') as tests:
 
-        #Additional
-        with open('../dados/additinal_final.txt', 'w') as out:
-            prio = file_tests[9].split(", ")
+        file_tests = tests.readlines()
 
-            for i in prio:
-                out.write(file_names[int(i)])
+        with open('../dados/tests-{}.txt'.format(post), 'r') as names:
 
-        #Echalon
-        with open('../dados/echalon_final.txt', 'w') as out:
-            prio = file_tests[15].split(", ")
+            file_names = names.readlines()
 
-            for i in prio:
-                out.write(file_names[int(i)])
-                
-        #Echalon Time
-        with open('../dados/echalon_time_final.txt', 'w') as out:
-            prio = file_tests[21].split(", ")
+            #Total
+            with open('../dados/total_final-{}.txt'.format(post), 'w') as out:
+                prio = file_tests[3].split(", ")
 
-            for i in prio:
-                out.write(file_names[int(i)])
+                for i in prio:
+                    out.write(file_names[int(i)])
 
-        #ARTMaxMin
-        with open('../dados/art_max_min_final.txt', 'w') as out:
-            prio = file_tests[27].split(", ")
+            #Additional
+            with open('../dados/additinal_final-{}.txt'.format(post),
+                      'w') as out:
+                prio = file_tests[9].split(", ")
 
-            for i in prio:
-                out.write(file_names[int(i)])
+                for i in prio:
+                    out.write(file_names[int(i)])
 
-        #Genetic
-        with open('../dados/genetic_final.txt', 'w') as out:
-            prio = file_tests[33].split(", ")
+            #Echalon
+            with open('../dados/echalon_final-{}.txt'.format(post),
+                      'w') as out:
+                prio = file_tests[15].split(", ")
 
-            for i in prio:
-                out.write(file_names[int(i)])
+                for i in prio:
+                    out.write(file_names[int(i)])
+
+            #Echalon Time
+            with open('../dados/echalon_time_final-{}.txt'.format(post),
+                      'w') as out:
+                prio = file_tests[21].split(", ")
+
+                for i in prio:
+                    out.write(file_names[int(i)])
+
+            #ARTMaxMin
+            with open('../dados/art_max_min_final-{}.txt'.format(post),
+                      'w') as out:
+                prio = file_tests[27].split(", ")
+
+                for i in prio:
+                    out.write(file_names[int(i)])
+
+            #Genetic
+            with open('../dados/genetic_final-{}.txt'.format(post),
+                      'w') as out:
+                prio = file_tests[33].split(", ")
+
+                for i in prio:
+                    out.write(file_names[int(i)])
+
+
+if __name__ == '__main__':
+    main()
